63265a79f7b99624404069bdeae53c54c179e5c7
Mariia <mariiapoliak1997@gmail.com>
2024-07-04 19:55:19 +0200

    commit message

--- src/main.js
+++ src/main.js
@@ -16,3 +16,22 @@ $(document).ready(function(){
     }
   });
 });
+
+// CATALOG
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	const showMoreBtn = document.querySelector('.catalog-button');
+
+	const hiddenItems = document.querySelectorAll('.catalog-item.hidden');
+	const catalogSection = document.getElementById('catalog');
+
+	showMoreBtn.addEventListener('click', function () {
+		hiddenItems.forEach(item => item.style.display = 'block');
+		showMoreBtn.style.display = 'none';
+		showLessBtn.style.display = 'block';
+	});
+
+		// Плавна прокрутка до секції Catalog
+		catalogSection.scrollIntoView({ behavior: 'smooth' });
+	});
