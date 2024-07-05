e4c85783b84c05a581c72cafdc5981b78c95a8a3
Anna Fleck <annafleck18@gmail.com>
2024-07-05 19:32:05 +0300

    fix-scroll

--- src/main.js
+++ src/main.js
@@ -1,37 +1,34 @@
-$(document).ready(function(){
-  
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
 
-    if (this.hash !== "") {
-     
-        event.preventDefault();
-        
       var hash = this.hash;
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
     }
   });
 });
 
 // CATALOG
 
-
 document.addEventListener('DOMContentLoaded', function () {
-	const showMoreBtn = document.querySelector('.catalog-button');
+  const showMoreBtn = document.querySelector('.catalog-button');
 
-	const hiddenItems = document.querySelectorAll('.catalog-item.hidden');
-	const catalogSection = document.getElementById('catalog');
+  const hiddenItems = document.querySelectorAll('.catalog-item.hidden');
+  const catalogSection = document.getElementById('catalog');
 
-	showMoreBtn.addEventListener('click', function () {
-		hiddenItems.forEach(item => item.style.display = 'block');
-		showMoreBtn.style.display = 'none';
-		showLessBtn.style.display = 'block';
-	});
-
-		// Плавна прокрутка до секції Catalog
-		catalogSection.scrollIntoView({ behavior: 'smooth' });
-	});
+  showMoreBtn.addEventListener('click', function () {
+    hiddenItems.forEach(item => (item.style.display = 'block'));
+    showMoreBtn.style.display = 'none';
+    showLessBtn.style.display = 'block';
+  });
+});
